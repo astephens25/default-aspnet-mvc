c72c4f0aaaeabbf6e91926eba6fb5ae8788a3c8c
Aaron Stephens <astephens@kimblelabs.com>
2016-07-23 16:58:39 +0100

    Add project files.

---
+++ DefaultAspnetMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DefaultAspnetMvc.MvcApplication" Language="C#" %>
---
+++ DefaultAspnetMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DefaultAspnetMvc.Startup))]
+namespace DefaultAspnetMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
